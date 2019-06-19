2881d6cb57744d71c91ee4f9378555d5b3a7bd0d
Rick Gibson <ricklesgibson@gmail.com>
2019-06-18 18:30:11 -0700

    Implement widget-add

--- src/app/widget-add/widget-add.component.ts
+++ src/app/widget-add/widget-add.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {ApiService} from '../api.service';
+import {FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-widget-add',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./widget-add.component.css']
 })
 export class WidgetAddComponent implements OnInit {
+  widgetForm: FormGroup;
+  title: string = '';
+  desc: string = '';
+  price: number = null;
+  date: Date = null;
+  isLoadingResults = false;
 
-  constructor() { }
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
+    this.widgetForm = this.formBuilder.group({
+      'title': [null, Validators.required],
+      'desc': [null, Validators.required],
+      'price': [null, Validators.required],
+      'date': [null, Validators.required]
+    });
+  }
+
+  onFormSubmit(form: NgForm) {
+    this.isLoadingResults = true;
+    this.api.addWidget(form)
+      .subscribe(res => {
+        /* tslint:disable:no-string-literal */
+        const id = res['_id'];
+        /* tslint:enable:no-string-literal */
+        this.isLoadingResults = false;
+        this.router.navigate(['.widget-info', id]);
+      }, (err) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
   }
 
 }
